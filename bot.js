33d61839418d41c81b311470790052d69e0a63fb
Tama Harrison <tamaharrison061@gmail.com>
2021-01-05 09:18:40 +0000

    Open bot to whole server

--- bot.js
+++ bot.js
@@ -1,15 +1,13 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const { BOT_TOKEN } = require('./config');
+const { BOT_TOKEN } = require("./config");
 
-client.on('ready', () => console.log('Bot is active'));
+client.on("ready", () => console.log("Bot is active"));
 
-client.on('message', (msg) => {
-
-  if ((msg.content == 'Ping' || msg.content == 'ping') && msg.channel.id == '775112162246787083') {
-      msg.reply('Pong');
+client.on("message", (msg) => {
+  if (msg.content == "Ping" || msg.content == "ping") {
+    msg.reply("Pong");
   }
-
 });
 
 client.login(BOT_TOKEN);
