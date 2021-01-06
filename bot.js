f04b7af3f5971340c74beeb56e7ae12953e2deec
Tama Harrison <tamaharrison061@gmail.com>
2021-01-06 10:05:41 +0000

    move login function

--- bot.js
+++ bot.js
@@ -5,83 +5,11 @@ const { BOT_TOKEN } = require("./config");
 client.on("ready", () => {
   console.log("ready");
 
-  client.api
-    .applications(client.user.id)
-    .guilds("775112162246787083")
-    .commands.post({
-      data: {
-        name: "hello",
-        description: "Replies with Hello World!",
-      },
-    });
-
-  client.api
-    .applications(client.user.id)
-    .guilds("699722023508770836")
-    .commands.post({
-      data: {
-        name: "echo",
-        description: "Echos your text as an embed!",
-
-        options: [
-          {
-            name: "content",
-            description: "Content of the embed",
-            type: 3,
-            required: true,
-          },
-        ],
-      },
-    });
-
-  client.ws.on("INTERACTION_CREATE", async (interaction) => {
-    const command = interaction.data.name.toLowerCase();
-    const args = interaction.data.options;
-
-    if (command == "hello") {
-      client.api.interactions(interaction.id, interaction.token).callback.post({
-        data: {
-          type: 4,
-          data: {
-            content: "Hello World!",
-          },
-        },
-      });
-    }
-
-    if (command == "echo") {
-      const description = args.find(
-        (arg) => arg.name.toLowerCase() == "content"
-      ).value;
-      const embed = new discord.MessageEmbed()
-        .setTitle("Echo!")
-        .setDescription(description)
-        .setAuthor(interaction.member.user.username);
-
-      client.api.interactions(interaction.id, interaction.token).callback.post({
-        data: {
-          type: 4,
-          data: await createAPIMessage(interaction, embed),
-        },
-      });
+  client.on("message", (msg) => {
+    if (msg.content == "Ping" || msg.content == "ping") {
+      msg.reply("Pong");
     }
   });
-});
-
-async function createAPIMessage(interaction, content) {
-  const apiMessage = await discord.APIMessage.create(
-    client.channels.resolve(interaction.channel_id),
-    content
-  )
-    .resolveData()
-    .resolveFiles();
-
-  return { ...apiMessage.data, files: apiMessage.files };
-}
-client.on("message", (msg) => {
-  if (msg.content == "Ping" || msg.content == "ping") {
-    msg.reply("Pong");
-  }
 });
 
 client.login(BOT_TOKEN);
